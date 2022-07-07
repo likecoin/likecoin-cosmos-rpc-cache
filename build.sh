9afcb622e30ceccdfa597857154a9fa79d50042a
WeiJun0827 <weijunhung@outlook.com>
2022-07-07 16:36:54 +0800

    🔨 Update build script for production

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ WD=`cd $(dirname "$0") && pwd -P`
 cd "${WD}"
 
 docker buildx build . -t like-cosmos-rpc-cache --platform linux/amd64
-docker tag like-cosmos-rpc-cache:latest us.gcr.io/likecoin-develop/like-cosmos-rpc-cache:latest
-docker -- push us.gcr.io/likecoin-develop/like-cosmos-rpc-cache:latest
+docker tag like-cosmos-rpc-cache:latest us.gcr.io/likecoin-foundation/like-cosmos-rpc-cache:latest
+docker -- push us.gcr.io/likecoin-foundation/like-cosmos-rpc-cache:latest
 
 cd "${PWD}"

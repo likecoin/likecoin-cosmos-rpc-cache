9a638d301dcdc7ab78bda8bcd6320e392c6a4100
William Chong <me@williamchong.cloud>
2022-07-19 19:20:27 +0800

    ➕ Add helment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require("helmet");
 
 const { RedisCache } = require('./cache');
 const { CachedJsonRpcProxy } = require('./proxy');
@@ -29,7 +30,7 @@ const proxy = new CachedJsonRpcProxy(rpcEndpoint, cache);
 proxy.addMatchers(...getMatchersFromConfig(matchersConfig));
 
 const app = express();
-
+app.use(helmet());
 app.get('/healthz', (req, res) => res.sendStatus(200));
 app.use(proxy.getExpressMiddleware());
 app.listen(listenAddr.port, listenAddr.hostname);
